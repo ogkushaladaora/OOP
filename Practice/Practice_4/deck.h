7c99c94d597f8726844beb2e9dfd1c0c91e90855
Daniel Ulinski <danielulinski@yahoo.com>
2019-03-06 19:34:49 -0500

    Update deck.h

--- Practice/Practice_4/deck.h
+++ Practice/Practice_4/deck.h
@@ -45,6 +45,7 @@ enum CardType {
 };
 
 class standardcard {
+public:
     standardcard(Rank r, Suit s)
     : rank(r), suit(s) {}
 
@@ -57,26 +58,51 @@ private:
 };
 
 class jokercard {
+public:
     jokercard(Color c)
     : color(c) {}
 
-    Color getcolor const {retrun color}
+    Color getcolor const {return color}
 
 private:
     Color color;
 };
 
-union card {
-    card(Rank r, Suit s)
+union kind {
+    kind(Rank r, Suit s)
     : standard(r, s) {}
 
-    card(Color C)
+    kind(Color C)
     : joker(c) {}
 
     standardcard standard;
     jokercard joker;
 };
 
+class card {
+public:
+    card(Rank r, Suit s)
+    : cardtype(Standard), cardkind(r, s) {}
 
+    card(Color c)
+    : cardtype(Joker), cardkind(c) {}
+
+private:
+    CardType cardtype;
+    kind cardkind;
+
+public:
+    //needs:
+    //way to tell if standard or joker
+    //way to return standard or joker elements
+
+    //standard and joker card determinator
+    Bool CardType() {
+        if (cardtype == Standard) {return false};
+        if (cardtype == Joker) {return true};
+    }
+
+    
+};
 
 #endif //P1_WAR_DECK_H
